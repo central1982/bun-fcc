e2798666538c69eb7c0670e5966b8d20787f570e
central1982 <central1982@gmail.com>
2025-02-10 20:06:52 +0100

    adding routes

--- index.ts
+++ index.ts
@@ -2,7 +2,20 @@ import figlet from 'figlet'
 const server = Bun.serve({
 	port: 3000,
 	fetch(req) {
-		return new Response('Hello world')
+		const url = new URL(req.url)
+
+		if (url.pathname == '/') {
+			const body = figlet.textSync('Hola mundo!!!')
+			return new Response(body)
+		}
+		if (url.pathname === '/about') {
+			return new Response('About me')
+		}
+		if (url.pathname === '/contact') {
+			return new Response('Contact us')
+		}
+
+		return new Response('404')
 	},
 })
 
